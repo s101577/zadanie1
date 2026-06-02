ea8e47a5388590422e4c48ba651bc5faa1274561
Szymon <s101577@pollub.edu.pl>
2026-06-02 23:34:37 +0200

    wersja 4 :)

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import datetime
 import logging
@@ -9,18 +8,17 @@ NAME = "Szymon Jagusiak"
 PORT = 8080
 START_DATE = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-
 logging.basicConfig(level=logging.INFO)
 print(f"--- LOG STARTOWY ---", flush=True)
 print(f"Data uruchomienia: {START_DATE}", flush=True)
 print(f"Autor: {NAME}", flush=True)
 print(f"Port TCP: {PORT}", flush=True)
 print(f"--------------------", flush=True)
+
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def weather_page(city: str = "Warszawa"):
-
     temp = "18°C" 
     return f"""
     <html>
@@ -41,48 +39,4 @@ async def weather_page(city: str = "Warszawa"):
             <footer>Autor: {NAME}</footer>
         </body>
     </html>
-=======
-import os
-import datetime
-import logging
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
-NAME = "Szymon Jagusiak"
-PORT = 8080
-START_DATE = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
-logging.basicConfig(level=logging.INFO)
-print(f"--- LOG STARTOWY ---", flush=True)
-print(f"Data uruchomienia: {START_DATE}", flush=True)
-print(f"Autor: {NAME}", flush=True)
-print(f"Port TCP: {PORT}", flush=True)
-print(f"--------------------", flush=True)
-app = FastAPI()
-
-@app.get("/", response_class=HTMLResponse)
-async def weather_page(city: str = "Warszawa"):
-
-    temp = "18°C" 
-    return f"""
-    <html>
-        <head><title>Pogoda - {NAME}</title></head>
-        <body>
-            <h1>Pogoda dla: {city}</h1>
-            <p>Temperatura: {temp}</p>
-            <p>Status: Słonecznie</p>
-            <hr>
-            <form action="/">
-                <select name="city">
-                  <option value="Warszawa">Warszawa</option>
-                  <option value="Kraków">Kraków</option>
-                  <option value="Gdańsk">Gdańsk</option>
-                </select>
-                <input type="submit" value="Sprawdź">
-            </form>
-            <footer>Autor: {NAME}</footer>
-        </body>
-    </html>
->>>>>>> 39890db4421ee0c480cdf971fee2bbf959217f16
     """
